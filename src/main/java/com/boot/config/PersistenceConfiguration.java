095f52551cc7222f67099188bfd461fbfdcae088
Waqar <waqaryshaikh@gmail.com>
2017-11-27 21:12:50 +0530

    Added JPA Support

--- src/main/java/com/boot/config/PersistenceConfiguration.java
+++ src/main/java/com/boot/config/PersistenceConfiguration.java
@@ -2,6 +2,7 @@ package com.boot.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,7 @@ public class PersistenceConfiguration {
 	
 	@Bean
 	@ConfigurationProperties(prefix="datasource.flyway")
+	@FlywayDataSource
 	public DataSource flywayDataSource(){
 		return DataSourceBuilder.create().build();
 	}
