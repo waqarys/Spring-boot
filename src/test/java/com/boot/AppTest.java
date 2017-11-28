8fe5d5fc5d5fa055ea1daec99d040a11f1c34ea5
shaikhwa <shaikhwa@MUMWD101061.ASIAPAC.NOM>
2017-11-28 12:58:28 +0530

    Added Junit support

--- src/test/java/com/boot/AppTest.java
+++ src/test/java/com/boot/AppTest.java
@@ -1,38 +1,18 @@
 package com.boot;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import com.boot.controller.HomeController;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+
+public class AppTest {
+	
+	@Test
+    public void testApp(){
+		HomeController hc = new HomeController();
+		String result =  hc.home();
+		assertEquals(result, "Das Boot, reporting for duty!");
     }
 }
